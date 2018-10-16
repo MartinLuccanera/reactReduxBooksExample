db273769dc56c7edad519d9e41d16bcaa498bebe
Martin Luccanera <martin.luccanera@jivesoftware.com>
2018-10-16 11:23:51 -0300

    Code added in Section 4 Lecture 42.

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
-
 /* NOTE: React and Redux are 2 separate libraries. In order to connect them we use react-redux. In order to do that,
  * we need to promote our component to a container. A container it's a react component that has a direct connection to
  * the state managed by redux. We usually place the containers in a new package.
+ * How do we make the decision to convert one into the other? It varies. In general we want the most parent component
+ * that cares about a particular piece of state to be a container. It could be the App, but in reality, the app, doesn't
+ * care about the list of books. Only BookList does. For another example, only BookDetails cares about the details of the book.
+ * The only thing that App is here for is to say "Hey render this to the page and that to the page".
+ * Hence, BookDetails and BookList should be containers (or smart components) and have access to redux.
+ * Remember: A container is just a component that has direct access to the state produced by Redux. React and Redux are separate libraries
+ * and it's only through a third library (react-redux) that we can meld the 2 of them together and get a component that is actually aware
+ * of the state that is contained within redux.
  */
 
+import React, { Component } from 'react';
 
 export default class BookList extends Component {
     renderList() {
