2cf9d30cbab6c08a89e22428740df41ae6c17f97
Martin Luccanera <martin.luccanera@gmail.com>
2018-10-23 09:23:35 -0300

    Code added in Section 4 Lecture 49.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,9 +4,9 @@ import BooksReducer from './reducer_books';
 import ActiveBook from './reducer_active_book';
 
 /*When we pass this object to CombinedReducers we are essentially telling redux how to create our app's state.
-* We would have a single piece of state called 'books' where the value is whatever gets returned from the BooksReducer function.
-*
-*/
+ * We would have a single piece of state called 'books' where the value is whatever gets returned from the BooksReducer function.
+ *
+ */
 const rootReducer = combineReducers({
     /*key: value (it's the reducer itself)*/
     books: BooksReducer,
